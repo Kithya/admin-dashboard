0f0ca033559a81b348dba7f9fc5ba81210aa2199
Kithya <narakithya@gmail.com>
2025-02-07 10:22:52 +0700

    completed user profile page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Set your desired port number
+    strictPort: true, // Prevents Vite from switching to another port
+  },
+});
